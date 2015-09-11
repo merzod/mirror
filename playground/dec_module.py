9bad45c1d2b3a166cb664e871f454e0919b899a3
merzod <merzod@gmail.com>
2015-09-11 17:27:53 +0000

    dec_module works

--- playground/dec_module.py
+++ playground/dec_module.py
@@ -3,10 +3,13 @@ import numpy
 import subprocess, os, sys
 
 def listen():
-    reccmd = ["ls", "-la"]
+    reccmd = ["arecord", "-D", "plughw:0,0", "-f", "cd", "-c", "1", "-t", "wav", "-d", "4", "-q", "-r", "16000"]
     proc = subprocess.Popen(reccmd, stdout=subprocess.PIPE)
+    print 'listening...'
     return proc.stdout.read()
 
-#data = listen()
-data = numpy.asarray(listen())
-scikits.audiolab.wavwrite(data, 't2.wav', fs=16000)
+data = listen()
+print 'decoding...'
+data2 = numpy.frombuffer(data, dtype=numpy.int16)
+
+scikits.audiolab.wavwrite(data2, 't2.wav', fs=16000)
