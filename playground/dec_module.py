782462b3496031d56ef8f80dd853e47bd2255307
merzod <merzod@gmail.com>
2015-09-11 21:19:05 +0300

    loop + silense threshold

--- playground/dec_module.py
+++ playground/dec_module.py
@@ -4,18 +4,31 @@ import subprocess, sys
 import logging
 from pocketsphinx import *
 from os import path
+import audioop
+
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s\t(%(threadName)-10s) %(filename)s:%(lineno)d\t%(message)s')
 
 MODELDIR = "/usr/local/share/pocketsphinx/model"
 DATADIR = "/home/pi"
+SEC = 4
+THRESHOLD = 100
 
 # function run 'arecord' console cmd for 'sec' seconds and gives back it's stdout
 def listen(sec):
     reccmd = ["arecord", "-D", "plughw:0,0", "-f", "cd", "-c", "1", "-t", "wav", "-d", "%s"%sec, "-q", "-r", "16000"]
     proc = subprocess.Popen(reccmd, stdout=subprocess.PIPE)
-    logging.info('Listening...')
     return proc.stdout.read()
+
+# function decode data using decoder, and return decoded string or None
+def process(decoder, data):
+    decoder.start_utt()
+    decoder.process_raw(data, False, False)
+    decoder.end_utt()
+    if decoder.hyp() is not None and decoder.hyp().hypstr:
+        return decoder.hyp().hypstr
+    return None
+
 
 # Create a decoder with certain model
 config = Decoder.default_config()
@@ -26,17 +39,15 @@ config.set_string('-samprate', '48000')
 config.set_string('-logfn', '/dev/null')
 decoder = Decoder(config)
 
-data = listen(4)
-logging.info('Processing...')
+while True:
+    logging.info('Listening...')
+    data = listen(SEC)
+    rms = audioop.rms(data, 2)
+    logging.debug('RMS: %d' % rms)
+    if rms > THRESHOLD:
+        str = process(decoder, data)
+        logging.info('Match: %s' % str)
+
 
 #data2file = numpy.frombuffer(data, dtype=numpy.int16)
 #scikits.audiolab.wavwrite(data, 't2.wav', fs=16000)
-
-decoder.start_utt()
-decoder.process_raw(data, False, False)
-decoder.end_utt()
-if decoder.hyp() is not None:
-    logging.info('Match: %s', decoder.hyp().hypstr)
-else:
-    logging.info('Nothing')
-
