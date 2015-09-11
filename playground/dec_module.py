ee3f7e6498a403d6c7744faabd2415e726fef136
merzod <merzod@gmail.com>
2015-09-11 16:38:46 +0300

    fix issue with cmd string

--- playground/dec_module.py
+++ playground/dec_module.py
@@ -7,6 +7,6 @@ def listen():
     proc = subprocess.Popen(reccmd, stdout=subprocess.PIPE)
     return proc.stdout.read()
 
-data = listen()
-#data = numpy.asarray(listen())
+#data = listen()
+data = numpy.asarray(listen())
 scikits.audiolab.wavwrite(data, 't2.wav', fs=16000)
