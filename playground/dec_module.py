acfa55879aba7629939a526c83b312ca4dd1bdec
merzod <merzod@gmail.com>
2015-09-11 20:53:45 +0300

    dec module listen for mic and decode

--- playground/dec_module.py
+++ playground/dec_module.py
@@ -1,15 +1,41 @@
 import scikits.audiolab
 import numpy
-import subprocess, os, sys
+import subprocess, sys
+import logging
+from pocketsphinx import *
+from os import path
 
-def listen():
-    reccmd = ["arecord", "-D", "plughw:0,0", "-f", "cd", "-c", "1", "-t", "wav", "-d", "4", "-q", "-r", "16000"]
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s\t(%(threadName)-10s) %(filename)s:%(lineno)d\t%(message)s')
+
+MODELDIR = "/usr/local/share/pocketsphinx/model"
+DATADIR = "/home/pi"
+
+# function run 'arecord' console cmd for 'sec' seconds and gives back it's stdout
+def listen(sec):
+    reccmd = ["arecord", "-D", "plughw:0,0", "-f", "cd", "-c", "1", "-t", "wav", "-d", "%s"%sec, "-q", "-r", "16000"]
     proc = subprocess.Popen(reccmd, stdout=subprocess.PIPE)
-    print 'listening...'
+    logging.info('Listening...')
     return proc.stdout.read()
 
-data = listen()
-print 'decoding...'
-data2 = numpy.frombuffer(data, dtype=numpy.int16)
+# Create a decoder with certain model
+config = Decoder.default_config()
+config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
+config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us/4986.lm'))
+config.set_string('-dict', path.join(MODELDIR, 'en-us/en-us/4986.dic'))
+config.set_string('-samprate', '48000')
+decoder = Decoder(config)
 
-scikits.audiolab.wavwrite(data2, 't2.wav', fs=16000)
+data = listen(4)
+logging.info('Processing...')
+
+#data2file = numpy.frombuffer(data, dtype=numpy.int16)
+#scikits.audiolab.wavwrite(data, 't2.wav', fs=16000)
+
+decoder.start_utt()
+decoder.process_raw(data, False, False)
+decoder.end_utt()
+if decoder.hyp() is not None:
+    logging.info('Match: %s', decoder.hyp().hypstr)
+else:
+    logging.info('Nothing')
+
