467833799a79c6bb2f4f6e1a485ae950dd32c9a9
merzod <merzod@gmail.com>
2015-10-09 15:59:35 +0300

    Turn added

--- hardware/base.py
+++ hardware/base.py
@@ -1,5 +1,6 @@
 import motor
 import threading
+import time
 
 
 class Base:
@@ -8,19 +9,21 @@ class Base:
 
     def __init__(self):
         self.m1 = motor.Motor(33, 35, 37)
-        self.m2 = motor.Motor(27, 29, 31)
+        self.m2 = motor.Motor(32, 29, 31)
 
     def move(self, direction=motor.Motor.FORWARD, period=1, speed=100):
         t1 = threading.Thread(target=self.m1.move, args=(direction, period, speed))
         t2 = threading.Thread(target=self.m2.move, args=(direction, period, speed))
         t1.start()
         t2.start()
+        time.sleep(period)
 
     def turn(self, direction=TURN_LEFT, period=1, speed=100):
         t1 = threading.Thread(target=self.m1.move, args=(not direction, period, speed))
         t2 = threading.Thread(target=self.m2.move, args=(direction, period, speed))
         t1.start()
         t2.start()
+        time.sleep(period)
 
 base = Base()
 base.move()
