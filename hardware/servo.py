e18dc15343bf5e870a96d2cbb355f8606a67950f
merzod <merzod@gmail.com>
2015-10-27 22:15:31 +0200

    servo improvement

--- hardware/servo.py
+++ hardware/servo.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import utils
 import logging
+import time
 
 
 # Class represents Servo. Hardware servo connected to PRi with single signal wire.
@@ -15,20 +16,17 @@ class Servo:
         self.min_angle = min_angle
         self.max_angle = max_angle
 
-        GPIO.setup(pin, GPIO.OUT)
-        self.p = GPIO.PWM(pin, 100)
-        self.p.start(1)
+        GPIO.setup(self.pin, GPIO.OUT)
+        self.p = GPIO.PWM(self.pin, 100)
 
     # Control the servo. Turn to specified angle.
     def move(self, angle):
         angle = utils.check(angle, self.MIN_ANGLE, self.MAX_ANGLE)
         val = float(angle) * 18.0 / 180.0 + 5
         logging.debug("Moving Servo angle=%d val=%d" % (angle, val))
-        self.p.ChangeDutyCycle(val)
-
-    def __del__(self):
+        self.p.start(val)
+        time.sleep(1)
         self.p.stop()
-
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
