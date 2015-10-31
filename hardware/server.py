480dd284a6c101d670568294de451c4cd4f0bfb6
merzod <merzod@gmail.com>
2015-10-31 17:32:20 +0200

    sound for server

--- hardware/server.py
+++ hardware/server.py
@@ -8,9 +8,11 @@ logging.basicConfig(level=logging.DEBUG,
 app = Flask(__name__)
 controller = Controller()
 
+
 @app.route("/")
 def hello():
     return render_template('form.html', **build_constants())
+
 
 @app.route("/<code>")
 def move(code):
@@ -22,20 +24,24 @@ def move(code):
         pass
     return render_template('form.html', **build_constants())
 
+
 def build_constants():
     map = {
-        'move_forward' : Controller.MOVE_FORWARD,
-        'move_backward' : Controller.MOVE_BACKWARD,
-        'turn_left' : Controller.TURN_LEFT,
-        'turn_right' : Controller.TURN_RIGHT,
-        'head_left' : Controller.HEAD_LEFT,
-        'head_right' : controller.HEAD_RIGHT,
-        'left_arm_up' : Controller.LEFT_ARM_UP,
-        'left_arm_down' : controller.LEFT_ARM_DOWN,
-        'right_arm_up' : Controller.RIGHT_ARM_UP,
-        'right_arm_down' : controller.RIGHT_ARM_DOWN
+        'move_forward': Controller.MOVE_FORWARD,
+        'move_backward': Controller.MOVE_BACKWARD,
+        'turn_left': Controller.TURN_LEFT,
+        'turn_right': Controller.TURN_RIGHT,
+        'head_left': Controller.HEAD_LEFT,
+        'head_right': controller.HEAD_RIGHT,
+        'left_arm_up': Controller.LEFT_ARM_UP,
+        'left_arm_down': controller.LEFT_ARM_DOWN,
+        'right_arm_up': Controller.RIGHT_ARM_UP,
+        'right_arm_down': controller.RIGHT_ARM_DOWN,
+        'sound_1': controller.SOUND_1,
+        'sound_2': controller.SOUND_2
     }
     return map
+
 
 if __name__ == "__main__":
     try:
