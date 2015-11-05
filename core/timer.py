76bb0d429a3e7e97d9bda4cb3e4ca52db5c5a59a
merzod <merzod@gmail.com>
2015-11-05 06:56:39 +0200

    formatting

--- core/timer.py
+++ core/timer.py
@@ -48,7 +48,7 @@ def secToFormat(time):
     hour = time / 3600
     min = s = time % 3600 / 60
     sec = time % 3600 % 60
-    return '%d:%d:%d' % (hour, min, sec)
+    return '%01d:%02d:%02d' % (hour, min, sec)
 
 
 # Getting localized name of the time unit
